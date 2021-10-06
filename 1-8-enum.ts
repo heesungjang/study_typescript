0f1e727c9a09ba03a11a64fa5e33081eaed26b7c
Heesung jang <heesungj7@gmail.com>
2021-10-06 23:11:17 +0900

    Create 1-8-enum.ts

---
+++ 1-8-enum.ts
@@ -0,0 +1,28 @@
+{
+    /**
+     * Enum
+     */
+
+    //JavaScript
+    const MAX_NUM = 6;
+    const MAX_STUDENT_PER_CLASS = 10;
+    const MONDAY = 0;
+    const TUESDAY = 1;
+    const WEDNESDAY = 2;
+    const DAYS_ENUM = Object.freeze({ MONDAY: 0, TUESDAY: 1, WEDNESDAY: 2 });
+    const dayOfToday = DAYS_ENUM.MONDAY;
+
+    // TypeScript
+    // enum에 값을 할당하지 않으면 index 처리로 값이 설정된다
+    enum DAYS {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday,
+    }
+    console.log(DAYS.Monday);
+    console.log(DAYS.Saturday);
+}
