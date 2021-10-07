9d6be1383c130b0e93d4ece3fc761c60490daeef
Heesung Jang <heesungj7@gmail.com>
2021-10-07 18:07:32 +0900

    Update 3-1-without-oop.ts

--- OOP/3-1-without-oop.ts
+++ OOP/3-1-without-oop.ts
@@ -1,12 +1,19 @@
+// 절자 지향적 프로그래밍
+
 {
+    // 커핕컵 타입 설정
     type CoffeeCup = {
         shots: number;
         hasMilk: boolean;
     };
 
+    // 샷 하나에 들어가는 커피 빈 그램 상수
     const BEANS_GRAMM_PER_SHOT: number = 7;
 
+    // 초기 커피 빈
     let coffeeBeans: number = 0;
+
+    // 커피 내리는 함수 작성
     function makeCoffee(shots: number): CoffeeCup {
         if (coffeeBeans < shots * BEANS_GRAMM_PER_SHOT) {
             throw new Error("Not enough coffee beans!");
