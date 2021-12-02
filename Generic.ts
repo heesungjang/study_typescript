2139f0b089003edfeb05d2a9125489777af05e13
Heesung Jang <heesungj7@gmail.com>
2021-12-02 18:16:08 +0900

    generic

--- Generic.ts
+++ Generic.ts
@@ -58,4 +58,13 @@ function conditionLogText<T>(text: T): T {
     console.log(text.length);
     return text;
   }
+
+
+  function getProperty<T, O extends keyof T>(obj: T, key: O) {
+    return obj[key];  
+  }
+  let obj = { a: 1, b: 2, c: 3 };
+  
+  getProperty(obj, "a"); // okay
+  getProperty(obj, "z"); // error: "z"는 "a", "b", "c" 속성에 해당하지 않습니다.
 }
