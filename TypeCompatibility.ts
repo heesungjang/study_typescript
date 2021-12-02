4d6f7c3d54ceaef34fdb866cbabb0d6302f5056b
Heesung Jang <heesungj7@gmail.com>
2021-12-02 18:20:08 +0900

    Type Compatibility

--- TypeCompatibility.ts
+++ TypeCompatibility.ts
@@ -15,5 +15,21 @@ interface Avengers {
 
 let hero: Avengers;
 // 타입스크립트가 추론한 y의 타입은 { name: string; location: string; } 입니다.
-let capt = { name: "Captain", location: "Pangyo" };
+let capt = { name: "Captain", location: "America" };
 hero = capt;
+
+class Hulk {
+  handSize: number;
+  constructor(name: string, numHand: number) {}
+}
+
+class Captain {
+  handSize: number;
+  constructor(numHand: number) {}
+}
+
+let a: Hulk;
+let s: Captain;
+
+a = s; // OK
+s = a; // OK
